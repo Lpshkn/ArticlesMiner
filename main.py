6dffd75334a488a2c09890183a5777f487d3087f
Lpshkn <lepkirill@yandex.ru>
2021-03-26 01:27:17 +0300

    Add the main module

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import sys
+from miner.console.handler import ConsoleHandler
+from miner.database.db import Database
+
+
 def main():
-    pass
+    console_handler = ConsoleHandler(sys.argv[1:])
+    try:
+        db = Database.connect([{'host': console_handler.host, 'port': console_handler.port}])
+    except ConnectionError:
+        print("The hostname or port of the Elasticsearch server is incorrect, please check it", file=sys.stderr)
+        exit(-1)
 
 
 if __name__ == '__main__':
