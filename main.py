6ef6d57c30a8ae1cd59770166837b5d21f1a138f
Lpshkn <lepkirill@yandex.ru>
2021-03-27 00:32:50 +0300

    Update the main function to run parsing of the habr.com

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import sys
 from miner.console.handler import ConsoleHandler
 from miner.database.db import Database
+from miner.habr.parser import Parser
 
 
 def main():
     console_handler = ConsoleHandler(sys.argv[1:])
+    db = None
     try:
         db = Database.connect([{'host': console_handler.host, 'port': console_handler.port}])
     except ConnectionError:
         print("The hostname or port of the Elasticsearch server is incorrect, please check it", file=sys.stderr)
         exit(-1)
+
+    parser = Parser(10, timeout=0)
+    parser.parse(database=db)
 
 
 if __name__ == '__main__':
